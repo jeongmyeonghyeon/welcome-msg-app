2ba90b4615460834c63215a57561fd95ece95c0d
hnblife-computer <hnblife-computer@hnblife-computerui-MacBook-Pro.local>
2019-02-07 17:50:45 +0900

    commit

---
+++ class-extend.js
@@ -0,0 +1,7 @@
+class Person {
+}
+// constructor(public string, public age) {
+// }
+Person.BLOOD_TYPE = '0';
+class Employee extends Person {
+}
---
+++ class-extend.ts
@@ -0,0 +1,10 @@
+class Person {
+    // constructor(public string, public age) {
+    // }
+    static BLOOD_TYPE: string = '0';
+}
+
+class Employee extends Person {
+    department: string;
+    role: string;
+}
